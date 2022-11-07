a6c4edafbf60eea24409d4597a5abb7811f9ca99
Mohamad <mohamad.shiro80026@gmail.com>
2022-11-07 10:59:24 +0200

    Final

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if (req.url === "/") {
-        var filePath = path.join(__dirname, "public", "server-page.html");
-    } else if (req.url === "/Template-1" || req.url === "/Template-1/") {
-        var filePath = path.join(__dirname, "public", req.url, "index.html");
+        var filePath = path.join(__dirname, "public", "index.html");
     } else {
         var filePath = path.join(__dirname, "public", req.url);
     }
@@ -36,9 +34,23 @@ const server = http.createServer((req, res) => {
     }
 
     fs.readFile(filePath, (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, `Contet-Type: ${contentType}`);
-        res.end(content);
+        if (err) {
+            if (err.code == "ENOENT") {
+              // Page not found
+              fs.readFile(path.join(__dirname, "public", "404.html"), (err, content) => {
+                    res.writeHead(404, { "Content-Type": "text/html" });
+                    res.end(content, "utf8");
+                });
+            } else {
+              //  Some server error
+              res.writeHead(500);
+              res.end(`Server Error: ${err.code}`);
+            }
+          } else {
+            // Success
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(content, "utf8");
+          }
     });
 
     
